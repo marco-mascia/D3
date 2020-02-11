95e43e031c6b7423526542e41e7fba5b5e5604fe
Marco Mascia [fabbricadigitale] <m.mascia@fabbricadigitale.it>
2020-02-11 22:05:42 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import './scss/main.scss';
+
+/*
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
+*/
+
+const messages = {
+    header: 'intestazione',
+    main: 'corpo',
+    aside: 'lato',
+    footer: 'piè di pagina'
+}
+
+export function init() {
+    /*
+   var src = document.getElementById('#print-script'); //$("#print-script").html();
+   var source = src.innerHTML();
+   var template = Handlebars.compile(source);
+   var data = {
+       image:        'http://placekitten.com/100/100',
+       title:        'title',
+       caption:      'caption',
+       category:     function() {
+           console.log('category');
+           return 'category';
+       } 
+   };
+   */
+
+   /*
+   let template = require("./index.handlebars");
+   let $compiled = template({});
+   $(document.body).append($compiled)
+   */
+
+}
+init();
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin'); //clean directories before build
+const isDevMode = 'production' !== process.env.NODE_ENV;
+
+module.exports = {
+    entry: { main: './src/main.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {   
+                test: /\.css$/i, 
+                use: ['style-loader', 'css-loader']
+            },
+            {   
+                test: /\.handlebars$/, 
+                loader: "handlebars-loader" 
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader','css-loader', 'postcss-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin('dist', {}),  //Cleans dist directory before build
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "style.[contenthash].css"
+        }),
+        new HtmlWebpackPlugin({
+            title: 'My Boilerplate',
+            template: './src/index.handlebars',
+            minify: !isDevMode && {
+                html5: true,
+                collapseWhitespace: true,
+                caseSensitive: true,
+                removeComments: true,
+                removeEmptyElements: true
+            },
+          }),
+        
+        new WebpackMd5Hash()
+    
+    ]
+};
